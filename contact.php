e0f3893df2f41cf89bc293f7d85ef2e2daa9a809
khatch2_name <khatchshah2@gmail.com>
2022-09-09 14:01:24 +0200

    2022-09-09

---
+++ contact.php
@@ -0,0 +1,14 @@
+// <!-- contact html -->
+echo "<!DOCTYPE html>";
+
+// <!-- <html lang="sv">
+echo "<head>";
+    <script src=engine.js"></script>
+    <link href="stylesheet.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    echo "<title>Välkommen till Contact</title>";
+echo "</head>";
+echo '<body class="contactPhp">';
+echo "</body>";
