237dac3601adf810561775d5d883c587237d019a
khatch2_name <khatchshah2@gmail.com>
2022-09-09 14:17:57 +0200

    här är det lite php-kod

--- contact.php
+++ contact.php
@@ -1,14 +1,3 @@
-// <!-- contact html -->
-echo "<!DOCTYPE html>";
-
-// <!-- <html lang="sv">
-echo "<head>";
-    <script src=engine.js"></script>
-    <link href="stylesheet.css" rel="stylesheet" />
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    echo "<title>Välkommen till Contact</title>";
-echo "</head>";
-echo '<body class="contactPhp">';
-echo "</body>";
+<?php
+echo "Här är lite php-kod.";
+?>
